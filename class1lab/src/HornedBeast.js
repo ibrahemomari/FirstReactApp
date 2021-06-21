3d8f1ee002932a6f8f698cdf173e5918805fff48
ibrahemomari <ibrahem.omari96@gmail.com>
2021-06-21 18:46:59 +0300

    class2 lab

--- class1lab/src/HornedBeast.js
+++ class1lab/src/HornedBeast.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 
 class HornedBeast extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            count:props.count
+        }
+    }
+    incremntLike=()=>{
+        this.setState({count:this.props.countRequest(this.state.count)})
+    }
     render() {
         return (
-            <div>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.image_url} alt="" title=""></img>
-                <p>{this.props.description}</p>
+            <div className="card">
+                <div className="card-header">{this.props.title}</div>
+                <div className="card-body"><img src={this.props.img_url} /></div>
+                <div className="card-footer">
+                <button className="btn btn-primary" onClick={this.incremntLike}>Vote</button>
+                <i class="uil uil-heart" ></i>
+                <span>{this.state.count}</span>
+
+            </div>
             </div>
         )
     }
